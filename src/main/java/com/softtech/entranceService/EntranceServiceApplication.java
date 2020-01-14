a7a936975fe8ab42bc36856d7825849cdacb6990
Bam <sbam@c02p5bxqg3qc.limited.brands.com>
2020-01-14 12:02:18 -0500

    inital commit with project sctructure

---
+++ src/main/java/com/softtech/entranceService/EntranceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.softtech.entranceService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntranceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntranceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softtech/entranceService/EntranceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softtech.entranceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntranceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
